1aaa0993fafe5b2b9e575e01c1def2fbb170765e
Adriano Bonat <adrianob@gmail.com>
2012-06-04 21:48:06 -0300

    refactored commonalities.

--- lib/bunch/cli.rb
+++ lib/bunch/cli.rb
@@ -2,51 +2,39 @@ require "thor"
 
 module Bunch
   class CLI < Thor
-    desc "clone", "Clone the repos specified in Bunchfile"
-    method_option :group,
-      :type    => :array,
-      :default => [],
-      :aliases => "-g",
-      :desc    => "Clone only repos in the groups"
-    def clone
-      groups = options[:group].map(&:to_sym)
-      definition = DSL.new(bunch_spec).to_definition
+    def self.group_option(desc)
+      method_option :group,
+        :type    => :array,
+        :default => [],
+        :aliases => "-g",
+        :desc    => desc
+    end
 
-      definition.repos_for(groups).each do |repo|
+    desc "clone", "Clone the repos specified in Bunchfile"
+    group_option "Clone only repos in the groups"
+    def clone
+      repos.each do |repo|
         Git.clone!(repo.spec)
       end
     end
 
     desc "foreach command", "Execute a command for each repo"
-    method_option :group,
-      :type    => :array,
-      :default => [],
-      :aliases => "-g",
-      :desc    => "Executes the command only for repos in the groups. Use -- to indicate the end of the group list and the start of the command to be executed."
+    group_option "Executes the command only for repos in the groups. Use -- to indicate the end of the group list and the start of the command to be executed."
     def foreach(*args)
-      groups = options[:group].map(&:to_sym)
       command = args.join(' ').gsub(/^\s*--?\s*/, '').strip
-
       raise "Command is empty!" if command.empty?
 
-      definition = DSL.new(bunch_spec).to_definition
-      definition.repos_for(groups).each do |repo|
+      repos.each do |repo|
         Kernel.system("cd #{repo.directory} && #{command}")
       end
     end
 
     desc "status", "Shows the repo working directory status"
-    method_option :group,
-      :type    => :array,
-      :default => [],
-      :aliases => "-g",
-      :desc    => "Shows the status only for the repos in the groups"
+    group_option "Shows the status only for the repos in the groups"
     def status
-      groups = options[:group].map(&:to_sym)
       command = "git status"
 
-      definition = DSL.new(bunch_spec).to_definition
-      definition.repos_for(groups).each do |repo|
+      repos.each do |repo|
         Kernel.system("cd #{repo.directory} && #{command}")
       end
     end
@@ -55,6 +43,14 @@ module Bunch
     def bunch_spec
       raise 'Bunchfile does not exist.' unless Bunch.file.file?
       Bunch.file.read
+    end
+
+    def repos
+      return @cached_repos if @cached_repos
+      groups = options[:group].map(&:to_sym)
+
+      definition = DSL.new(bunch_spec).to_definition
+      @cached_repos = definition.repos_for(groups)
     end
   end
 end
